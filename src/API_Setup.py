4592d43138c28460e338344e65cc07a48d5c4be2
Christianfirecup <Christianfirecup@aol.com>
2025-09-09 19:30:39 -0400

    removed dupicate prompt

--- src/API_Setup.py
+++ src/API_Setup.py
@@ -4,10 +4,7 @@ from dotenv import load_dotenv
 from agents import Agent, Runner
 import asyncio
 
-"You exist to look for a context of conversation between an ai streamer streaming with the main human streamer plus"
-                 " input from chat snice we have a limited amount of tokens you need to pick the most relevant and most self identifying conversations in these files"
-                 "to upload as context for next stream youll be handed 21 files and you pick the most non essential file to pass in since well"
-                 " be always passing in 20 txt files if you conclude that all files are important "
+
 load_dotenv()
 OAIkey = os.getenv("OAI")
 
